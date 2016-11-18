b1f7e1d1841660fc63c930202197fadaa2230b43
kr-g <kr-g>
2016-11-18 07:56:05 +0100

    first checkin

---
+++ setenv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export ANDROID_HOME=~/android-sdk-linux/
+export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
+echo '$ANDROID_HOME'=$ANDROID_HOME
+echo '$PATH'=$PATH
+
